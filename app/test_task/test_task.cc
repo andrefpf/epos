a2db57a8fefe62862224884ad727e64fe0edf15e
Hans Buss <hans.buss@mailfence.com>
2022-02-15 15:18:03 -0300

    3-1 tests

--- app/test_task/test_task.cc
+++ app/test_task/test_task.cc
@@ -30,7 +30,7 @@ int test_1() {
     int id = Task::self()->id();
 
     if (id == 0) {
-        cout << "Starting test 1" << endl;
+    	cout << "=====Starting test 1=====" << endl;
         fork(&test_1);
         fork(&test_1);
     } else {
@@ -42,10 +42,94 @@ int test_1() {
     return 0;
 }
 
+int test_2()
+{
+    cout << "=====Starting test 2=====" << endl;
+    cout << "Task Addres Space and Segment test" << endl;
+    System_Info * si = System::info();
+    cout << "App location in System Info \n" 
+         << " App Code Segment :" << CPU::Phy_Addr(si->pmm.app_code) << "\n"
+         << " App Data Segment :" << CPU::Phy_Addr(si->pmm.app_data)
+         << endl;
+
+    Task * task0 = Task::self();
+    Address_Space * as0 = task0->address_space();
+    cout << "\nMy address space's page directory is located at " << as0->pd() << endl;
+
+    Segment * cs0 = task0->code_segment();
+    CPU::Log_Addr code0 = task0->code();
+    CPU::Phy_Addr code_phy = as0->physical(code0);
+    cout << "\nMy code segment is : " << "\n"
+         << "Logical Addr: " << static_cast<void *>(code0) << "\n"
+         << "Physical Addr: " << static_cast<void *>(code_phy) << "\n"
+         << "Size: " << cs0->size() << " bytes long" << endl;
+
+    // Assert if Logical Memory is Valid in Code Segment
+    cout << "\nAssert if logic code is valid" << endl;
+    assert(code0 == Memory_Map::APP_CODE);
+
+    Segment * ds0 = task0->data_segment();
+    CPU::Log_Addr data0 = task0->data();
+    CPU::Phy_Addr data_phy = as0->physical(data0);
+    cout << "\nMy data segment is : " << "\n"
+         << "Logical Addr: " << static_cast<void *>(data0) << "\n"
+         << "Physical Addr: " << static_cast<void *>(data_phy) << "\n"
+         << "Size: " << ds0->size() << " bytes long" << endl;
+    
+    // Assert if Logical Memory is Valid in Data Segment
+    cout << "\nAssert if logic data is valid" << endl;
+    assert(data0 == Memory_Map::APP_DATA);
+
+    // Assert if the Physicall memory data is located with correct space from code
+    cout << "Assert if phy data has size needed" << endl;
+    assert((data_phy + ds0->size()) == code_phy);
+
+    cout << "I'm done, bye!" << endl;
+	    return 0;
+}
+
+int func_a() {
+    cout << "Nǐ hǎo wǒ shì “rènwù”: "<< Task::self()->id() << endl;
+    int step = 1000000;
+    for (int i = 0; i < 100000000; ++i) {
+      if (!(i % step)) {
+        ASM("_func_a_h:");
+        cout << "Checkpoint! ("<<Task::self()->id() <<")("<<i/step<<")" << endl;
+        // Alarm::delay(100000);
+      }
+    }
+    cout << "Task " << Task::self()->id() << "Shuō zàijiàn!" << endl;
+    return 0;
+}
+
+int test_3()
+{  
+    cout << "=====Starting test 3=====" << endl;
+    cout << "=====Test Task Timer "<< Task::self()->id()<<"=====" << endl;
+
+    if (Task::self()->id() == 0) {
+        fork(&func_a);
+        fork(&func_a);
+        cout << "Hello World! I'm Task: "<< Task::self()->id() << endl;
+    }
+    if (Task::self()->id() == 1) {
+        cout << "Nǐ hǎo shìjiè wǒ shì rènwù: "<< Task::self()->id() << endl;
+	func_a();
+    }
+    if (Task::self()->id() == 2) {
+        cout << "Annyeong haseyo wǒ shì rènwù: "<< Task::self()->id() << endl;
+        func_a();
+    }
+
+    print_inf();
+    cout << "Zàijiàn! onz" << endl;
+    return 0;
+}
 
 int main()
 {
     test_1();
-
+    test_2();
+    test_3();
     return 0;
 }
