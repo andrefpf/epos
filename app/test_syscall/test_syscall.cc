e37ae2e4ae8283872561acf8db17a52b243e8e6d
Andre Fernandes <fpf.andre@gmail.com>
2022-02-25 20:05:56 -0300

    criando meu primeiro stub

--- app/test_syscall/test_syscall.cc
+++ app/test_syscall/test_syscall.cc
@@ -1,5 +1,6 @@
 #include <utility/ostream.h>
 #include <syscall/message.h>
+#include <syscall/stubs.h>
 
 using namespace EPOS;
 
@@ -7,14 +8,27 @@ OStream cout;
 
 void test_1() {
     // Printa alguma coisa através de mensagens
-    Message msg(0, Message::ENTITY::DISPLAY, Message::PRINT, "Teste 1 está funcionando");
+    Message msg(0, Message::ENTITY::DISPLAY, Message::PRINT, "Teste 1 está funcionando \n");
     msg.act();
 }
 
+void test_2() {
+    // Usa o stubs common pra printar algo
+    stubs::Stubs_Common display(Message::ENTITY::DISPLAY);
+    display.invoke(Message::PRINT, "Teste 2 está funcionando \n");
+}
+
+void test_3() {
+    // usa o stub display para printar algo
+    stubs::Display display = stubs::Display();
+    display.print("Teste 3 está funcionando \n");
+}
 
 int main()
 {
     test_1();
+    test_2();
+    test_3();
 
     // CPU::syscall(0);
 
